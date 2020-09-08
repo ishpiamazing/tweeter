1e663094119e69b218a88d1020fa2e823a21ae37
ishpiamazing <ishpiamazing@gmail.com>
2020-09-08 20:56:00 +0000

    Implemented the character counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('#tweet-text').on('keyup keypress', function () {
+    // Traverse the DOM up and down to form to find the closest element with .counter class set
+    let counter = $(this).closest("form").find(".counter");
+    // Set Maximum Length of the tweet
+    let maxlen = 140;
+    // Available Character Count variable for the user to input
+    let charsLeft = maxlen - $(this).val().length;
+    //  Display the remaining characters
+    counter.text(charsLeft);
+    // If Available Character Count variable for the user exceeds 140, change the color to red
+    if(charsLeft < 0){
+      counter.addClass("fontRed");
+    }else{
+      counter.removeClass("fontRed");
+    }
+  });
+});
+
+
